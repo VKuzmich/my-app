1756b3b46ca1e2bcc3158c8b035b8787b164efc8
Vladimir Kuzmich <vjk1976@ukr.net>
2021-03-02 10:07:34 +0200

    00039 props and state

--- src/App.js
+++ src/App.js
@@ -3,14 +3,23 @@ import './App.css';
 import Person from './Person/Person';
 
 class App extends Component {
+    state = {
+        persons: [
+            { name: 'Ford', age: "12" },
+            { name: 'Dan', age: "22" },
+            { name: 'Kan', age: "32" }
+        ]
+    }
+
     render() {
       return (
           <div className="App">
             <h1>Hi! i am react app!!!</h1>
             <p>Just paragraph!!!</p>
-            <Person name="Dariy" age='43'/>
-            <Person name="Friend" age='23'> My hobbies: hiking! </Person>
-            <Person name="Mica" age='12'/>
+            <button>Switch Name</button>
+            <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
+            <Person name={this.state.persons[1].name} age={this.state.persons[1].age}> My hobbies: hiking! </Person>
+            <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
           </div>
       );
       //   return React.createElement('div',  {className: 'App'}, React.createElement('h1', null, 'Hi, i\'m React App!!!') )
