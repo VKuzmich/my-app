e3b39aae3b9736144d5f51f331cb8d3c384ceefd
Vladimir Kuzmich <vjk1976@ukr.net>
2021-03-03 16:07:58 +0200

    00054 conditionally handling dynamic content

--- src/App.js
+++ src/App.js
@@ -72,6 +72,24 @@ class App extends Component {
           cursor: 'pointer'
       };
 
+      let persons = null
+
+      if (this.state.showPersons) {
+          persons = (
+              <div>
+
+                  <Person name={this.state.persons[0].name}
+                          age={this.state.persons[0].age}/>
+                  <Person name={this.state.persons[1].name}
+                          age={this.state.persons[1].age}
+                          click={this.switchNameHandler.bind(this, 'Firm')}
+                          changed={this.nameChangedHandler}> My hobbies: hiking! </Person>
+                  <Person name={this.state.persons[2].name}
+                          age={this.state.persons[2].age}/>
+
+              </div>
+          )
+      }
 
       return (
           <div className="App">
@@ -80,20 +98,9 @@ class App extends Component {
             <button
                 style={style}
                 onClick={this.togglePersonsHandler}>Switch Name</button>
+              {persons}
 
-              {this.state.showPersons === true ?
-                  <div>
 
-                      <Person name={this.state.persons[0].name}
-                              age={this.state.persons[0].age}/>
-                      <Person name={this.state.persons[1].name}
-                              age={this.state.persons[1].age}
-                              click={this.switchNameHandler.bind(this, 'Firm')}
-                              changed={this.nameChangedHandler}> My hobbies: hiking! </Person>
-                      <Person name={this.state.persons[2].name}
-                          age={this.state.persons[2].age}/>
-                  </div> : null
-              }
           </div>
       );
       //   return React.createElement('div',  {className: 'App'}, React.createElement('h1', null, 'Hi, i\'m React App!!!') )
