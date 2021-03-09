3d4f1e29481309dfb9ec412432778515fd902168
Vladimir Kuzmich <vjk1976@ukr.net>
2021-03-09 15:15:35 +0200

    00072 styled button with styled components

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import './App.css';
 import Person from './Person/Person';
 
 const StyledButton = styled.button`
-  background-color: green;
+  background-color: ${props => props.alt ? 'red' : 'green'};
   color: white;
   font: inherit;
   border: 2px solid blue;
@@ -13,7 +13,7 @@ const StyledButton = styled.button`
   cursor: pointer;
   
   &:hover {
-      background-color: lightgreen;
+      background-color: ${props => props.alt ? 'salmon' : 'lightgreen'};
       color: black;
   }
 `;
@@ -135,7 +135,7 @@ class App extends Component {
               <div className="App">
                 <h1>Hi! i am react app!!!</h1>
                 <p className={classes.join(' ')}>Just paragraph!!!</p>
-                <StyledButton
+                <StyledButton alt={this.state.showPersons}
 
                     onClick={this.togglePersonsHandler}>Switch Name</StyledButton>
                   {persons}
